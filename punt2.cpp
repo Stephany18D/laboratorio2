9cb0fd0f5c2d9b3dff8db7191d69b0ec3758d3e8
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-03-17 17:35:30 -0500

    puntos

--- punt2.cpp
+++ punt2.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
+#include <ctime>
 
 int p2() {
     using namespace std;
 
-    char letras[10];
+    const int tamano = 10;
+    char letras[tamano];
     int conteo[26] = {0};
 
+    srand(time(nullptr));
 
-    for(int i = 0; i < 10; i++) {
-        letras[i] = 'A' + rand() % 26; // Genera una letra aleatoria mayúscula
+    cout << "Letras generadas aleatoriamente: ";
+    for (int i = 0; i < tamano; i++) {
+        letras[i] = 'A' + rand() % 26;
         cout << letras[i];
-
 
         conteo[letras[i] - 'A']++;
     }
 
-    cout << endl;
-
-
-    for(int i = 0; i < 26; i++) {
-        if(conteo[i] > 0) {
+    cout << "\n\nFrecuencia de cada letra:\n";
+        for (int i = 0; i < 26; ++i) {
             char letra = 'A' + i;
-            cout << letra << ": " << conteo[i] << endl;
-        }
+            if (conteo[i] > 0) {
+                cout << letra << ": " << conteo[i] << endl;
+            }
     }
 
     return 0;
