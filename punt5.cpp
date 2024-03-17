cd7e209266cc193987ce9148a3020ee8a513ce38
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-03-17 18:10:41 -0500

    puntos

---
+++ punt5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void cadena1(int numero, string& cadena) {
+    cadena = to_string(numero);
+}
+
+int p5() {
+    int numero;
+    cout << "Ingrese un numero entero: ";
+    cin >> numero;
+
+    string cadenaResultado;
+
+    cadena1(numero, cadenaResultado);
+    cout << "Cadena resultante: " << cadenaResultado << endl;
+
+    return 0;
+}
