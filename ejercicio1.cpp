bc6ee6069c5a9189da9ff75dcbb6431a93961b9b
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-03-20 19:28:52 -0500

    puntos - lab

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -27,3 +27,14 @@ void fun_b(int a[], int tam) {
         fun_a(&b[f], &b[i]);
     }
 }
+//∙ ¿Cuál es su dirección en memoria?
+    // La direccion en memoria es @0x70fdc0
+//¿Cuántos bytes se dedican para almacenar  cada elemento de array?
+    // Cada elemento de array ocupan 4 bytes
+//∙ ¿Cuál es la dirección y el contenido en memoria del elemento array [3]?
+    //La direccion y el contenido en memoria del elemento array [3] es 0 x 401580.
+//∙ Describe el efecto que tiene la función fun_b, sobre el arreglo array.
+    //La función fun_b invierte el orden de los elementos del arreglo array. Para poder lograr esto, se hace lo siguiente:
+    //- Se crea un puntero b que apunta al inicio del arreglo array.
+    //- Se recorre el arreglo desde los extremos hacia adentro utilizando un bucle for.
+    //- Y en cada iteración del bucle, llama a la función fun_a para intercambiar los valores de los elementos que se encuentran en los índices f e i.
